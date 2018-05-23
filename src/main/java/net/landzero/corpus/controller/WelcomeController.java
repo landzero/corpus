784f19fbbdd3468af18c497df87d3b16ef2c9587
etanaru <etanaru@landzero.net>
2018-05-23 17:52:25 +0800

    initial spring-boot project

---
+++ src/main/java/net/landzero/corpus/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package net.landzero.corpus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/health_check")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
